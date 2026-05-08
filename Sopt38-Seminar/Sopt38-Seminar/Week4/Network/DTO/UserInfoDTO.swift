3f63e5960a61ad4f16febe5fab3fb25bc62b7b44
hemssy <hemsyeons@gmail.com>
2026-05-08 20:47:06 +0900

    [Feat] #8 - 개인정보 DTO 추가

---
+++ Sopt38-Seminar/Sopt38-Seminar/Week4/Network/DTO/UserInfoDTO.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct UserInfoResponseDTO: Decodable {
+    let success: Bool
+    let status: Int
+    let message: String
+    let code: String
+    let data: UserInfoData?
+    let meta: ErrorResponseDTO?
+}
+
+struct UserInfoData: Decodable {
+    let id: Int
+    let loginId: String
+    let name: String
+    let email: String
+    let age: Int
+    let part: String
+}
