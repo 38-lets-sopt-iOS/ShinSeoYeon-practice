1e95ffb4b91f5926d8ac2b267fa9a8ce7c7b2602
hemssy <hemsyeons@gmail.com>
2026-05-08 21:09:35 +0900

    [Design] #8 - UserInfoViewController UI 수정

--- Sopt38-Seminar/Sopt38-Seminar/Week4/ViewControllers/UserInfoViewController.swift
+++ Sopt38-Seminar/Sopt38-Seminar/Week4/ViewControllers/UserInfoViewController.swift
@@ -4,74 +4,127 @@ import SnapKit
 import Then
 
 final class UserInfoViewController: UIViewController {
+    
     private let userId: Int
-
+    
     private let titleLabel = UILabel()
-    private let userInfoTextView = UITextView()
-
+    
+    private let loginIdTextField = UITextField()
+    private let nameTextField = UITextField()
+    private let emailTextField = UITextField()
+    private let ageTextField = UITextField()
+    private let partTextField = UITextField()
+    
+    private let editButton = UIButton()
+    
     init(userId: Int) {
         self.userId = userId
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUI()
         setStyle()
         setLayout()
         getUserInfo()
     }
-
+    
     private func setUI() {
         view.addSubviews(
             titleLabel,
-            userInfoTextView
+            loginIdTextField,
+            nameTextField,
+            emailTextField,
+            ageTextField,
+            partTextField,
+            editButton
         )
     }
-
+    
     private func setStyle() {
         view.backgroundColor = .white
-
+        
         titleLabel.do {
             $0.text = "내 정보 조회 및 수정"
-            $0.font = .systemFont(ofSize: 20, weight: .semibold)
+            $0.font = .systemFont(ofSize: 25, weight: .semibold)
             $0.textColor = .black
         }
-
-        userInfoTextView.do {
-            $0.font = .systemFont(ofSize: 15)
-            $0.textColor = .black
-            $0.isEditable = false
-            $0.layer.borderWidth = 1
-            // $0.layer.borderColor = .systemGray.cgColor 에러
+        
+        [loginIdTextField,
+         nameTextField,
+         emailTextField,
+         ageTextField,
+         partTextField]
+            .forEach {
+                $0.do {
+                    $0.borderStyle = .roundedRect
+                    $0.font = .systemFont(ofSize: 15)
+                    $0.addLeftPadding()
+                    $0.backgroundColor = .white
+                }
+            }
+        
+        loginIdTextField.isEnabled = false
+        partTextField.isEnabled = false
+        
+        editButton.do {
+            $0.setTitle("수정하기", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.backgroundColor = .systemBlue
             $0.layer.cornerRadius = 10
-            $0.textContainerInset = UIEdgeInsets(
-                top: 15,
-                left: 10,
-                bottom: 15,
-                right: 10
-            )
+            $0.titleLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
         }
     }
-
+    
     private func setLayout() {
         titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(100)
-            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.leading.equalToSuperview().inset(20)
         }
-
-        userInfoTextView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+        
+        loginIdTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
             $0.horizontalEdges.equalToSuperview().inset(20)
-            $0.height.equalTo(200)
+            $0.height.equalTo(55)
+        }
+        
+        nameTextField.snp.makeConstraints {
+            $0.top.equalTo(loginIdTextField.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+        }
+        
+        emailTextField.snp.makeConstraints {
+            $0.top.equalTo(nameTextField.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+        }
+        
+        ageTextField.snp.makeConstraints {
+            $0.top.equalTo(emailTextField.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+        }
+        
+        partTextField.snp.makeConstraints {
+            $0.top.equalTo(ageTextField.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+        }
+        
+        editButton.snp.makeConstraints {
+            $0.top.equalTo(partTextField.snp.bottom).offset(40)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
         }
     }
-
+    
     private func getUserInfo() {
         Task {
             do {
@@ -87,18 +140,15 @@ final class UserInfoViewController: UIViewController {
             }
         }
     }
-
+    
     private func updateUI(user: UserInfoData) {
-        userInfoTextView.text = """
-        id: \(user.id)
-        아이디: \(user.loginId)
-        이름: \(user.name)
-        이메일: \(user.email)
-        나이: \(user.age)
-        파트: \(user.part)
-        """
+        loginIdTextField.text = user.loginId
+        nameTextField.text = user.name
+        emailTextField.text = user.email
+        ageTextField.text = "\(user.age)"
+        partTextField.text = user.part
     }
-
+    
     private func showAlert(
         title: String,
         message: String
@@ -108,10 +158,14 @@ final class UserInfoViewController: UIViewController {
             message: message,
             preferredStyle: .alert
         )
-
-        let okAction = UIAlertAction(title: "확인", style: .default)
+        
+        let okAction = UIAlertAction(
+            title: "확인",
+            style: .default
+        )
+        
         alert.addAction(okAction)
-
+        
         present(alert, animated: true)
     }
 }
